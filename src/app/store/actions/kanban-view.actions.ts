6a7c5c97b82db831d3d39547f23ed0e6265f587e
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-02-27 15:59:07 -0600

    More actions for the kanban view

--- src/app/store/actions/kanban-view.actions.ts
+++ src/app/store/actions/kanban-view.actions.ts
@@ -1,9 +1,15 @@
 import { createActionGroup, props } from '@ngrx/store';
-import { Project } from '../../models';
+import { Project, Section, Task } from '../../models';
 
 export const KanbanViewActions = createActionGroup({
   source: 'Kanban View',
   events: {
     'Switch to List View': props<{ project: Project }>(),
+    'Add Task': props<{ task: Task }>(),
+    'Add Section': props<{ section: Section }>(),
+    'Delete Task': props<{ task: Task }>(),
+    'Delete Section': props<{ section: Section }>(),
+    'Update Task': props<{ task: Task }>(),
+    'Update Section': props<{ section: Section }>(),
   },
 });
