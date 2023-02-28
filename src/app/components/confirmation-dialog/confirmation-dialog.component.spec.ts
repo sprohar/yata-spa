81f261013d8da0adfdec40379fdc4aa498b2d11a
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-02-28 11:41:23 -0600

    Unit test: confirmation dialog

--- src/app/components/confirmation-dialog/confirmation-dialog.component.spec.ts
+++ src/app/components/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -1,16 +1,43 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ConfirmationDialogData } from '../../interfaces/confirmation-dialog-data';
 
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
 
 describe('ConfirmationDialogComponent', () => {
   let component: ConfirmationDialogComponent;
   let fixture: ComponentFixture<ConfirmationDialogComponent>;
+  let mockDialogRef: jasmine.SpyObj<MatDialogRef<ConfirmationDialogComponent>>;
+  let dialogData: ConfirmationDialogData = {
+    title: 'Confirm action',
+    content: 'Are you sure?',
+  };
 
   beforeEach(async () => {
+    mockDialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
+
     await TestBed.configureTestingModule({
-      declarations: [ ConfirmationDialogComponent ]
-    })
-    .compileComponents();
+      declarations: [ConfirmationDialogComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [NoopAnimationsModule, MatDialogModule, MatButtonModule],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: dialogData,
+        },
+        {
+          provide: MatDialogRef,
+          useValue: mockDialogRef,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ConfirmationDialogComponent);
     component = fixture.componentInstance;
@@ -19,5 +46,17 @@ describe('ConfirmationDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have data', () => {
+    expect(component.data).toBeDefined();
+    expect(component.data).toEqual(dialogData);
+  });
+
+  describe('#handleConfirm', () => {
+    it('should call close() on the dialogRef', () => {
+      component.handleConfirm();
+      expect(mockDialogRef.close).toHaveBeenCalled();
+    });
   });
 });
