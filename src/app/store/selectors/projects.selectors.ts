23d6999dbbd13a41a5ca43692495b86df78c2951
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-02-28 00:08:49 -0600

    Added a selector to get the project's sections

--- src/app/store/selectors/projects.selectors.ts
+++ src/app/store/selectors/projects.selectors.ts
@@ -1,13 +1,36 @@
 import { createSelector } from '@ngrx/store';
-import { Project } from '../../models';
+import { Project, Section, Task } from '../../models';
 import {
   selectCurrentProjectId,
   selectProjects,
 } from '../reducers/projects.reducer';
+import { selectTasks } from '../reducers/tasks.reducer';
 
 export const selectCurrentProject = createSelector(
   selectCurrentProjectId,
   selectProjects,
   (projectId: number | null, projects: Project[]) =>
     projects.find((p) => p.id === projectId)
+);
+
+export const selectProjectSections = createSelector(
+  selectCurrentProject,
+  selectTasks,
+  (project: Project | undefined, tasks: Task[]) => {
+    if (!project || !project.sections) {
+      return [] as Section[];
+    }
+
+    const sections: Section[] = [];
+    for (const section of project.sections) {
+      sections.push({
+        ...section,
+        tasks: tasks.filter(
+          (task) => task.sectionId && task.sectionId === section.id
+        ),
+      });
+    }
+
+    return sections;
+  }
 );
