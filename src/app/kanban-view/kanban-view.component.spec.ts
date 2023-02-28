b6e9037af8cd008c19ec36200ae9ed4546079348
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-02-28 16:00:43 -0600

    Initial setup for KanbanView spec

--- src/app/kanban-view/kanban-view.component.spec.ts
+++ src/app/kanban-view/kanban-view.component.spec.ts
@@ -1,23 +1,54 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { AppState } from '../store/app.state';
 
 import { KanbanViewComponent } from './kanban-view.component';
+
+const initialState: AppState = {
+  projects: {
+    currentProjectId: 1,
+    projects: [{ id: 1, name: 'Project' }],
+  },
+  sections: {
+    currentSectionId: 1,
+    sections: [{ id: 1, name: 'Section', projectId: 1 }],
+  },
+  tasks: {
+    currentTaskId: null,
+    tasks: [{ id: 1, title: 'Task', sectionId: 1, projectId: 1 }],
+  },
+};
 
 describe('KanbanViewComponent', () => {
   let component: KanbanViewComponent;
   let fixture: ComponentFixture<KanbanViewComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ KanbanViewComponent ]
-    })
-    .compileComponents();
+      declarations: [KanbanViewComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [MatButtonModule, MatIconModule],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(KanbanViewComponent);
+    store = TestBed.inject(MockStore);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#handleAddKanbanColumnComponentClosed', () => {
+    it('should set the flag to false', () => {
+      component.handleAddKanbanColumnComponentClosed();
+      expect(component.showAddKanbanColumnComponent).toBe(false);
+    });
   });
 });
