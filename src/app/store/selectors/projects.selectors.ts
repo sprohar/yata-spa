fbe5cc000b7ae1f3c45f832fef58c6b7eb89d2d4
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-03 11:53:30 -0600

    Moved "selectKanbanColumns"

--- src/app/store/selectors/projects.selectors.ts
+++ src/app/store/selectors/projects.selectors.ts
@@ -1,30 +1,13 @@
 import { createSelector } from '@ngrx/store';
-import { Project, Section } from '../../models';
+import { Project } from '../../models';
 import {
   selectCurrentProjectId,
   selectProjects,
 } from '../reducers/projects.reducer';
-import { selectSections } from '../reducers/sections.reducer';
-import { selectTasks } from '../reducers/tasks.reducer';
 
 export const selectCurrentProject = createSelector(
   selectCurrentProjectId,
   selectProjects,
   (projectId: number | null, projects: Project[]) =>
     projects.find((p) => p.id === projectId)
-);
-
-export const selectKanbanColumns = createSelector(
-  selectSections,
-  selectTasks,
-  (sections, tasks) => {
-    const collection: Section[] = [];
-    for (const section of sections) {
-      collection.push({
-        ...section,
-        tasks: tasks.filter((task) => task.sectionId === section.id),
-      });
-    }
-    return collection;
-  }
 );
--- src/app/store/selectors/sections.selectors.ts
+++ src/app/store/selectors/sections.selectors.ts
@@ -1,11 +1,33 @@
 import { createSelector } from '@ngrx/store';
+import { Section } from '../../models';
+import { selectCurrentProjectId } from '../reducers/projects.reducer';
 import {
   selectCurrentSectionId,
   selectSections,
 } from '../reducers/sections.reducer';
+import { selectTasks } from '../reducers/tasks.reducer';
 
 export const selectCurrentSection = createSelector(
   selectCurrentSectionId,
   selectSections,
   (sectionId, sections) => sections.find((s) => s.id === sectionId)
+);
+
+export const selectKanbanColumns = createSelector(
+  selectCurrentProjectId,
+  selectSections,
+  selectTasks,
+  (projectId, sections, tasks) => {
+    const collection: Section[] = [];
+    for (const section of sections) {
+      collection.push({
+        ...section,
+        tasks: tasks.filter(
+          (task) =>
+            task.sectionId === section.id && task.projectId === projectId
+        ),
+      });
+    }
+    return collection;
+  }
 );
