7ce8f90eee8c81a78efe3243b837c1303e0dfbe6
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-05 13:21:29 -0600

    Added components to the ListViewComponent

--- src/app/list-view/list-view.component.spec.ts
+++ src/app/list-view/list-view.component.spec.ts
@@ -1,18 +1,39 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { AppState } from '../store/app.state';
 
 import { ListViewComponent } from './list-view.component';
+
+const initialState: AppState = {
+  projects: {
+    currentProjectId: 1,
+    projects: [{ id: 1, name: 'Project' }],
+  },
+  sections: {
+    currentSectionId: null,
+    sections: [{ id: 1, name: 'Section', projectId: 1 }],
+  },
+  tasks: {
+    currentTaskId: null,
+    tasks: [{ id: 1, title: 'Task', projectId: 1, sectionId: 1 }],
+  },
+};
 
 describe('ListViewComponent', () => {
   let component: ListViewComponent;
   let fixture: ComponentFixture<ListViewComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListViewComponent ]
-    })
-    .compileComponents();
+      declarations: [ListViewComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ListViewComponent);
+    store = TestBed.inject(MockStore);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/list-view/list-view.component.ts
+++ src/app/list-view/list-view.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Section, Task } from '../models';
+import {
+  selectCompletedTasks,
+  selectSectionsWithIncompleteTasks,
+  selectUnsectionedTasks,
+} from '../store/selectors';
 
 @Component({
   selector: 'yata-list-view',
   templateUrl: './list-view.component.html',
-  styleUrls: ['./list-view.component.scss']
+  styleUrls: ['./list-view.component.scss'],
 })
 export class ListViewComponent {
+  completedTasks$ = this.store.select(selectCompletedTasks);
+  unsectionedTasks$ = this.store.select(selectUnsectionedTasks);
+  sections$ = this.store.select(selectSectionsWithIncompleteTasks);
 
+  constructor(private store: Store) {}
+
+  createCompletedSection(completedTasks: Task[]) {
+    return {
+      name: 'Completed',
+      tasks: completedTasks,
+      projectId: completedTasks[0].projectId,
+    } as Section;
+  }
 }
