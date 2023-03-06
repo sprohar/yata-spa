264448330f47158c1a8e31a092429f0f49cd4410
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-05 20:29:42 -0600

    Added test for ConfirmationDialogService

--- src/app/services/confirmation-dialog.service.spec.ts
+++ src/app/services/confirmation-dialog.service.spec.ts
@@ -1,16 +1,40 @@
 import { TestBed } from '@angular/core/testing';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 import { ConfirmationDialogService } from './confirmation-dialog.service';
 
 describe('ConfirmationDialogService', () => {
   let service: ConfirmationDialogService;
+  let matDialogSpy: jasmine.SpyObj<MatDialog>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    matDialogSpy = jasmine.createSpyObj('MatDialog', ['open']);
+    
+    TestBed.configureTestingModule({
+      imports: [MatDialogModule],
+      providers: [
+        {
+          provide: MatDialog,
+          useValue: matDialogSpy,
+        }
+      ]
+    });
+
     service = TestBed.inject(ConfirmationDialogService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('#open', () => {
+    it('should open the dialog', () => {
+      service.open({
+        content: 'content',
+        title: 'title',
+      });
+
+      expect(matDialogSpy.open).toHaveBeenCalled();
+    })
+  })
 });
