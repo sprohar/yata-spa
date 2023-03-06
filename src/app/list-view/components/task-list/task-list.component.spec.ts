488d9a475b755007807321cee1a8c2a5db89652c
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-06 10:48:12 -0600

    Modified the task list component to use the task-list-item component

--- src/app/list-view/components/task-list/task-list.component.spec.ts
+++ src/app/list-view/components/task-list/task-list.component.spec.ts
@@ -8,10 +8,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatRippleModule } from '@angular/material/core';
 import { MatDividerModule } from '@angular/material/divider';
-import { Router } from '@angular/router';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { Section, Task } from '../../../models';
-import { ListViewActions } from '../../../store/actions';
 
 import { TaskListComponent } from './task-list.component';
 
@@ -21,11 +19,8 @@ describe('TaskListComponent', () => {
   let component: TaskListComponent;
   let fixture: ComponentFixture<TaskListComponent>;
   let store: MockStore;
-  let router: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
-    router = jasmine.createSpyObj('Router', ['navigate']);
-
     await TestBed.configureTestingModule({
       declarations: [TaskListComponent],
       imports: [
@@ -34,7 +29,7 @@ describe('TaskListComponent', () => {
         MatDividerModule,
         DragDropModule,
       ],
-      providers: [provideMockStore(), { provide: Router, useValue: router }],
+      providers: [provideMockStore()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TaskListComponent);
@@ -54,70 +49,6 @@ describe('TaskListComponent', () => {
       component.tasks = tasks;
       fixture.detectChanges();
       expect(component).toBeTruthy();
-    });
-  });
-
-  describe('handleSelectedTask', () => {
-    beforeEach(() => {
-      component = fixture.componentInstance;
-      component.tasks = tasks;
-      fixture.detectChanges();
-    });
-
-    it('should navigate to "/list/:projectId/tasks/:taskId"', () => {
-      const task = tasks[0];
-      const routerNavigateSpy = router.navigate as jasmine.Spy;
-
-      component.handleSelectedTask(task);
-
-      const navArgs = routerNavigateSpy.calls.first().args[0];
-      const expectedNavArgs = ['list', task.projectId, 'tasks', task.id];
-
-      expect(navArgs).toEqual(expectedNavArgs);
-    });
-  });
-
-  describe('#handleCheckbox', () => {
-    beforeEach(() => {
-      component = fixture.componentInstance;
-      component.tasks = tasks;
-      fixture.detectChanges();
-    });
-
-    it('should dispatch the "markTaskAsComplete" action', () => {
-      spyOn(store, 'dispatch');
-      const task = tasks[0];
-      const isChecked = true;
-
-      component.handleCheckbox(isChecked, task);
-
-      expect(store.dispatch).toHaveBeenCalledWith(
-        ListViewActions.markTaskAsComplete({
-          task: {
-            id: task.id,
-            projectId: task.projectId,
-            completed: isChecked,
-          },
-        })
-      );
-    });
-
-    it('should dispatch the "markTaskAsIncomplete" action', () => {
-      spyOn(store, 'dispatch');
-      const task = tasks[0];
-      const isChecked = false;
-
-      component.handleCheckbox(isChecked, task);
-
-      expect(store.dispatch).toHaveBeenCalledWith(
-        ListViewActions.markTaskAsIncomplete({
-          task: {
-            id: task.id,
-            projectId: task.projectId,
-            completed: isChecked,
-          },
-        })
-      );
     });
   });
 
--- src/app/list-view/components/task-list/task-list.component.ts
+++ src/app/list-view/components/task-list/task-list.component.ts
@@ -1,9 +1,9 @@
 import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Section, Task } from '../../../models';
 import { ListViewActions } from '../../../store/actions';
+import { selectCurrentSection } from '../../../store/selectors';
 
 @Component({
   selector: 'yata-task-list',
@@ -13,40 +13,13 @@ import { ListViewActions } from '../../../store/actions';
 export class TaskListComponent implements OnInit {
   @Input() section?: Section;
   @Input() tasks!: Task[];
+  currentSection$ = this.store.select(selectCurrentSection);
 
-  constructor(private store: Store, private router: Router) {}
+  constructor(private store: Store) {}
 
   ngOnInit(): void {
     if (!this.tasks) {
       throw new Error('"tasks" is undefined');
-    }
-  }
-
-  handleSelectedTask(task: Task) {
-    this.router.navigate(['list', task.projectId, 'tasks', task.id]);
-  }
-
-  handleCheckbox(checked: boolean, task: Task) {
-    if (checked) {
-      this.store.dispatch(
-        ListViewActions.markTaskAsComplete({
-          task: {
-            id: task.id,
-            completed: true,
-            projectId: task.projectId,
-          },
-        })
-      );
-    } else {
-      this.store.dispatch(
-        ListViewActions.markTaskAsIncomplete({
-          task: {
-            id: task.id,
-            completed: false,
-            projectId: task.projectId,
-          },
-        })
-      );
     }
   }
 
