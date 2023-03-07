3d4d46797a23fb3a0c4862d6a645ee34159c2b0e
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-07 00:26:14 -0600

    Fixed errors in SubtasksService spec

--- src/app/services/subtasks.service.spec.ts
+++ src/app/services/subtasks.service.spec.ts
@@ -1,13 +1,22 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { SubtasksService } from './subtasks.service';
 
 describe('SubtasksService', () => {
   let service: SubtasksService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
     service = TestBed.inject(SubtasksService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
