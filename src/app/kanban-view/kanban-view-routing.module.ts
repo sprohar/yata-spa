a64566a19f5e7d653878fc0d59b335fa4f5deffa
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-10 16:27:55 -0600

    Removed the TaskDetailsDialog from the routing modules

--- src/app/kanban-view/kanban-view-routing.module.ts
+++ src/app/kanban-view/kanban-view-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { projectTasksGuard } from '../guards/project-tasks.guard';
-import { taskResolver } from '../resolvers/task-details.resolver';
-import { TaskDetailsComponent } from '../shared/components/task-details/task-details.component';
 import { KanbanViewComponent } from './kanban-view.component';
 
 const routes: Routes = [
@@ -10,15 +8,7 @@ const routes: Routes = [
     path: ':projectId',
     component: KanbanViewComponent,
     canActivate: [projectTasksGuard],
-  },
-  {
-    path: ':projectId/tasks/:taskId',
-    component: TaskDetailsComponent,
-    canActivate: [projectTasksGuard],
-    resolve: {
-      task: taskResolver,
-    }
-  },
+  }
 ];
 
 @NgModule({
--- src/app/list-view/list-view-routing.module.ts
+++ src/app/list-view/list-view-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { projectTasksGuard } from '../guards/project-tasks.guard';
-import { taskResolver } from '../resolvers/task-details.resolver';
-import { TaskDetailsComponent } from '../shared/components/task-details/task-details.component';
 import { ListViewComponent } from './list-view.component';
 
 const routes: Routes = [
@@ -10,14 +8,6 @@ const routes: Routes = [
     path: ':projectId',
     component: ListViewComponent,
     canActivate: [projectTasksGuard],
-  },
-  {
-    path: ':projectId/tasks/:taskId',
-    component: TaskDetailsComponent,
-    canActivate: [projectTasksGuard],
-    resolve: {
-      task: taskResolver,
-    },
   },
 ];
 
