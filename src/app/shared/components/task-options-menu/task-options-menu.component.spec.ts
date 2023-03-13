814cc2ea92eb9efa5cb2f30165b4bc1053107908
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-13 00:00:10 -0500

    Added the MockStore to task-options spec

--- src/app/shared/components/task-options-menu/task-options-menu.component.spec.ts
+++ src/app/shared/components/task-options-menu/task-options-menu.component.spec.ts
@@ -1,18 +1,37 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 import { TaskOptionsMenuComponent } from './task-options-menu.component';
 
 describe('TaskOptionsMenuComponent', () => {
   let component: TaskOptionsMenuComponent;
   let fixture: ComponentFixture<TaskOptionsMenuComponent>;
+  let dialog: jasmine.SpyObj<MatDialog>;
+  let store: MockStore;
 
   beforeEach(async () => {
+    dialog = jasmine.createSpyObj('MatDialog', ['open']);
+
     await TestBed.configureTestingModule({
-      declarations: [ TaskOptionsMenuComponent ]
-    })
-    .compileComponents();
+      declarations: [TaskOptionsMenuComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [MatDialogModule, MatMenuModule, MatButtonModule, MatIconModule],
+      providers: [
+        provideMockStore(),
+        {
+          provide: MatDialog,
+          useValue: dialog,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TaskOptionsMenuComponent);
+    store = TestBed.inject(MockStore);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
