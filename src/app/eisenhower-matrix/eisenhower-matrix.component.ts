9b548f9ec33663932a508578c41d0564ee3289c5
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-13 15:58:41 -0500

    Populate the quadrants with tasks

--- src/app/eisenhower-matrix/eisenhower-matrix.component.ts
+++ src/app/eisenhower-matrix/eisenhower-matrix.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { Store } from '@ngrx/store';
+import {
+  selectHighPriorityTasks,
+  selectLowPriorityTasks,
+  selectMediumPriorityTasks,
+  selectNoPriorityTasks,
+} from '../store/selectors';
 
 @Component({
   selector: 'yata-eisenhower-matrix',
   templateUrl: './eisenhower-matrix.component.html',
-  styleUrls: ['./eisenhower-matrix.component.scss']
+  styleUrls: ['./eisenhower-matrix.component.scss'],
 })
-export class EisenhowerMatrixComponent {
+export class EisenhowerMatrixComponent implements OnInit {
+  noPriorityTasks$ = this.store.select(selectNoPriorityTasks);
+  lowPriorityTasks$ = this.store.select(selectLowPriorityTasks);
+  mediumPriorityTasks$ = this.store.select(selectMediumPriorityTasks);
+  highPriorityTasks$ = this.store.select(selectHighPriorityTasks);
 
-  constructor(private store: Store) { }
+  constructor(private store: Store, private title: Title) {}
+
+  ngOnInit(): void {
+    this.title.setTitle('Eisenhower Matrix - Yata');
+  }
 }
--- src/app/store/selectors/tasks.selectors.ts
+++ src/app/store/selectors/tasks.selectors.ts
@@ -1,4 +1,5 @@
 import { createSelector } from '@ngrx/store';
+import { Task } from '../../models';
 import { selectCurrentTaskId, selectTasks } from '../reducers/tasks.reducer';
 
 export const selectCurrentTask = createSelector(
@@ -13,4 +14,20 @@ export const selectUnsectionedTasks = createSelector(selectTasks, (tasks) =>
 
 export const selectCompletedTasks = createSelector(selectTasks, (tasks) =>
   tasks.filter((task) => task.completed)
+);
+
+export const selectHighPriorityTasks = createSelector(selectTasks, (tasks) =>
+  tasks.filter((t) => t.priority === Task.Priority.HIGH)
+);
+
+export const selectMediumPriorityTasks = createSelector(selectTasks, (tasks) =>
+  tasks.filter((t) => t.priority === Task.Priority.MEDIUM)
+);
+
+export const selectLowPriorityTasks = createSelector(selectTasks, (tasks) =>
+  tasks.filter((t) => t.priority === Task.Priority.LOW)
+);
+
+export const selectNoPriorityTasks = createSelector(selectTasks, (tasks) =>
+  tasks.filter((t) => t.priority === Task.Priority.NONE)
 );
