89fa6b6ac2037c6a62f878823e90913591e498e7
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-14 18:17:50 -0500

    Added the auth effects to the auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { AuthEffects } from '../store/effects';
 import { authFeature } from '../store/reducers/auth.reducer';
 
 @NgModule({
-  imports: [CommonModule, StoreModule.forFeature(authFeature)],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(authFeature),
+    EffectsModule.forFeature([AuthEffects]),
+  ],
 })
 export class AuthModule {}
