aab82ebb9ea878bd6510494afe4b3492fa4d4d90
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-14 19:16:34 -0500

    Added an auth guard

---
+++ src/app/auth/guards/authentication.guard.ts
@@ -0,0 +1,28 @@
+import { inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Store } from '@ngrx/store';
+import { catchError, of, switchMap } from 'rxjs';
+import { selectIsAuthenticated } from '../../store/reducers/auth.reducer';
+
+export function authenticationGuard(
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) {
+  const store = inject(Store);
+  const router = inject(Router);
+
+  return store.select(selectIsAuthenticated).pipe(
+    switchMap((isAuthenticated) => {
+      if (isAuthenticated) {
+        return of(true);
+      }
+      router.navigateByUrl('/sign-in');
+      return of(false);
+    }),
+    catchError(() => of(false))
+  );
+}
