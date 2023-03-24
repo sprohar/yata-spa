d0d69aaf305e0665588a325664f6cf60ebfe25b8
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-24 15:12:47 -0500

    Feature: Update a tag

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
 import { CreateTagDialogComponent } from '../../tags/components/create-tag-dialog/create-tag-dialog.component';
@@ -9,13 +9,16 @@ import { selectUser } from '../../store/reducers/auth.reducer';
 import { selectProjects, selectTags } from '../../store/selectors';
 import { CreateProjectDialogComponent } from '../create-project-dialog/create-project-dialog.component';
 import { EditTagDialogComponent } from '../../tags/components/edit-tag-dialog/edit-tag-dialog.component';
+import { ConfirmationDialogService } from '../../services/confirmation-dialog.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'yata-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
+export class MainComponent implements OnDestroy {
+  private readonly destroy$ = new Subject<void>();
   projects$ = this.store.select(selectProjects);
   tags$ = this.store.select(selectTags);
   isHandset$ = this.breakpointService.isHandset$;
@@ -27,10 +30,15 @@ export class MainComponent {
   constructor(
     private breakpointService: BreakpointService,
     private store: Store,
-    private dialog: MatDialog
-  ) {}
+    private dialog: MatDialog,
+    private confirmationDialog: ConfirmationDialogService
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.destroy$.next();
+  }
+
+  ngOnInit(): void { }
 
   logout() {
     this.store.dispatch(AuthActions.logout());
@@ -56,5 +64,27 @@ export class MainComponent {
     this.dialog.open(EditTagDialogComponent, {
       data: tag,
     });
+  }
+
+  openDeleteTagConfirmationDialog(tag: Tag) {
+    const ref = this.confirmationDialog.open({
+      title: 'Confirmation',
+      content: 'Delete tag?',
+      confirmButtonColor: 'warn',
+      confirmButtonText: 'Delete',
+    });
+
+    ref
+      .afterClosed()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((result) => {
+        if (result) {
+          this.store.dispatch(
+            SidenavActions.deleteTag({
+              tag,
+            })
+          );
+        }
+      });
   }
 }
