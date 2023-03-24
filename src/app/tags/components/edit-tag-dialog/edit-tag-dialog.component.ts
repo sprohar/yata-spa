74f42fe13c98c50971ada4e4b0ce9004191eec8a
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-24 00:08:28 -0500

    Created the edit-tag-dialog component

---
+++ src/app/tags/components/edit-tag-dialog/edit-tag-dialog.component.ts
@@ -0,0 +1,53 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { Tag } from 'src/app/models';
+import { SidenavActions } from 'src/app/store/actions';
+
+@Component({
+  selector: 'yata-edit-tag-dialog',
+  templateUrl: './edit-tag-dialog.component.html',
+  styleUrls: ['./edit-tag-dialog.component.scss'],
+})
+export class EditTagDialogComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(
+    private store: Store,
+    @Inject(MAT_DIALOG_DATA)
+    private data: Tag,
+    private dialogRef: MatDialogRef<EditTagDialogComponent>,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.initForm(this.data);
+  }
+
+  private initForm(tag: Tag) {
+    this.form = this.fb.group({
+      id: [tag.id, [Validators.required]],
+      name: [
+        tag.name,
+        [Validators.required, Validators.maxLength(Tag.Name.MaxLength)],
+      ],
+      colorHexCode: [
+        tag.colorHexCode,
+        [Validators.maxLength(Tag.ColorHexCode.MaxLength)],
+      ],
+    });
+  }
+
+  handleSave() {
+    if (this.form.invalid || this.form.pristine) return;
+
+    this.store.dispatch(
+      SidenavActions.editTag({
+        tag: this.form.value,
+      })
+    );
+
+    this.dialogRef.close();
+  }
+}
