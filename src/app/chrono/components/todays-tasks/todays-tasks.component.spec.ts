ed30e5ac920caba8985437cddebd8a6c50a78e0d
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-03-30 17:41:45 -0500

    Import errors in spec

--- src/app/chrono/components/todays-tasks/todays-tasks.component.spec.ts
+++ src/app/chrono/components/todays-tasks/todays-tasks.component.spec.ts
@@ -1,18 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { TodaysTasksComponent } from './todays-tasks.component';
+import { MatDividerModule } from '@angular/material/divider';
 
 describe('TodaysTasksComponent', () => {
   let component: TodaysTasksComponent;
   let fixture: ComponentFixture<TodaysTasksComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodaysTasksComponent ]
-    })
-    .compileComponents();
+      declarations: [TodaysTasksComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [MatDividerModule],
+      providers: [provideMockStore()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TodaysTasksComponent);
+    store = TestBed.inject(MockStore);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
