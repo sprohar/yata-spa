0790c4f0af3b4fad15a4694e8fdfb9f744aa6d9f
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-04-02 13:48:04 -0500

    Unit tests: Section

--- src/app/shared/components/section/section.component.spec.ts
+++ src/app/shared/components/section/section.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { SectionComponent } from './section.component';
 
 describe('SectionComponent', () => {
@@ -8,9 +10,9 @@ describe('SectionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SectionComponent ]
-    })
-    .compileComponents();
+      declarations: [SectionComponent],
+      imports: [MatButtonModule, MatIconModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SectionComponent);
     component = fixture.componentInstance;
@@ -19,5 +21,29 @@ describe('SectionComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a name', () => {
+    component.name = 'Test';
+    expect(component.name).toBeTruthy();
+  });
+
+  it('should have an items count', () => {
+    component.itemsCount = 1;
+    expect(component.itemsCount).toBeTruthy();
+  });
+
+  it('should have an expanded state', () => {
+    expect(component.expanded).toBeTruthy();
+  });
+
+  it('should toggle expanded state', () => {
+    component.toggleExpanded();
+    expect(component.expanded).toBeFalsy();
+  });
+
+  it('should have a toggle button', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.clickable')).toBeTruthy();
   });
 });
