9c82edffe4879877e43677d4673b9d4ac854dab2
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-04-08 00:50:14 -0500

    Refactored the task-details resolver

--- src/app/resolvers/task-details.resolver.ts
+++ src/app/resolvers/task-details.resolver.ts
@@ -1,34 +1,35 @@
 import { inject } from '@angular/core';
 import {
+  ActivatedRoute,
   ActivatedRouteSnapshot,
   ResolveFn,
+  Router,
   RouterStateSnapshot,
 } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { tap } from 'rxjs';
+import { catchError, EMPTY, tap } from 'rxjs';
+import { ApiErrorResponse } from '../error';
 import { Task } from '../models';
 import { TasksService } from '../services/tasks.service';
-import { TaskResolverActions, YataApiActions } from '../store/actions';
+import { YataApiActions } from '../store/actions';
 
 export const taskResolver: ResolveFn<Task> = (
   route: ActivatedRouteSnapshot,
   _state: RouterStateSnapshot
 ) => {
   const store = inject(Store);
-  const projectId = parseInt(route.paramMap.get('projectId')!);
+  const projectId = parseInt(route.parent?.paramMap.get('projectId')!);
   const taskId = parseInt(route.paramMap.get('taskId')!);
-
-  if (taskId) {
-    store.dispatch(TaskResolverActions.setCurrentTaskId({ taskId }));
-  }
-
+  const router = inject(Router);
+  const thisRoute = inject(ActivatedRoute);
   return inject(TasksService)
     .get(projectId, taskId)
     .pipe(
-      tap((task) => {
-        if (task) {
-          store.dispatch(YataApiActions.loadTaskSuccess({ task }));
-        }
+      tap((task) => store.dispatch(YataApiActions.loadTaskSuccess({ task }))),
+      catchError((error: ApiErrorResponse) => {
+        store.dispatch(YataApiActions.loadTaskError({ error }));
+        router.navigate(['../..'], { relativeTo: thisRoute });
+        return EMPTY;
       })
     );
 };
