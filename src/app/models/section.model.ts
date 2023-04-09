e54cf46b1158d70a4ed33b1aabf85ad1f9013370
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-04-09 17:37:54 -0500

    Added dragDropData to the Section model
    This will be used to determine which fields to update when a task is dragged & dropped between sections

--- src/app/models/section.model.ts
+++ src/app/models/section.model.ts
@@ -1,10 +1,18 @@
-import { Task } from './task.model';
+import { Priority, Task } from './task.model';
+
+type DragDropTargetData = {
+  priority?: Priority;
+  projectId?: number;
+  dueDate?: string | null;
+  sectionId?: number | null;
+};
 
 export interface Section {
   id?: number;
   name: string;
   projectId: number;
   tasks?: Task[];
+  dragDropData?: DragDropTargetData;
 }
 
 export namespace Section {
