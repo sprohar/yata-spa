2b4e8170bdd40b105b97bb02e8d915c31f169796
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-04-10 00:02:14 -0500

    Added the task-details resolver to the chrono-routing module

--- src/app/chrono/chrono-routing.module.ts
+++ src/app/chrono/chrono-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { taskDetailsResolver } from '../resolvers/task-details.resolver';
+import { TaskDetailsDialogEntryComponent } from '../shared/components/task-details-dialog-entry/task-details-dialog-entry.component';
 import { ChronoComponent } from './chrono.component';
 import { NextSevenDaysComponent } from './components/next-seven-days/next-seven-days.component';
 import { TodaysTasksComponent } from './components/todays-tasks/todays-tasks.component';
@@ -15,11 +17,29 @@ const routes: Routes = [
         path: 'today',
         component: TodaysTasksComponent,
         canActivate: [todaysTasksGuard],
+        children: [
+          {
+            path: 'tasks/:taskId',
+            component: TaskDetailsDialogEntryComponent,
+            resolve: {
+              task: taskDetailsResolver,
+            },
+          },
+        ],
       },
       {
         path: 'upcoming',
         component: NextSevenDaysComponent,
         canActivate: [nextSevenDaysTasksGuard],
+        children: [
+          {
+            path: 'tasks/:taskId',
+            component: TaskDetailsDialogEntryComponent,
+            resolve: {
+              task: taskDetailsResolver,
+            },
+          },
+        ],
       },
     ],
   },
