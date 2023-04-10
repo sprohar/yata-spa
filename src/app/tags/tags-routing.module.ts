a9854b14337b5c594325bd6d2f7d2af4618f901e
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-04-09 23:43:17 -0500

    Open the task-details-dialog component from the tags view

--- src/app/tags/tags-routing.module.ts
+++ src/app/tags/tags-routing.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
+import { taskDetailsResolver } from '../resolvers/task-details.resolver';
+import { TaskDetailsDialogEntryComponent } from '../shared/components/task-details-dialog-entry/task-details-dialog-entry.component';
 import { taggedTasksGuard } from './guards/tagged-tasks.guard';
 import { TagsComponent } from './tags.component';
 
 const routes: Route[] = [
   {
-    path: ':tagId/tasks',
+    path: ':tagId',
     component: TagsComponent,
     canActivate: [taggedTasksGuard],
+    children: [
+      {
+        path: 'tasks/:taskId',
+        component: TaskDetailsDialogEntryComponent,
+        resolve: {
+          task: taskDetailsResolver,
+        },
+      },
+    ],
   },
 ];
 
@@ -15,4 +26,4 @@ const routes: Route[] = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class TagsRoutingModule { }
+export class TagsRoutingModule {}
--- src/app/tags/tags.module.ts
+++ src/app/tags/tags.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
-import { CreateTagDialogComponent } from './components/create-tag-dialog/create-tag-dialog.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { TagsComponent } from './tags.component';
-import { TagsRoutingModule } from './tags-routing.module';
-import { SharedModule } from '../shared/shared.module';
-import { StoreModule } from '@ngrx/store';
-import { tagsFeature } from '../store/reducers/tags.reducer';
-import { TagsEffects } from '../store/effects';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { EditTagDialogComponent } from './components/edit-tag-dialog/edit-tag-dialog.component';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { SharedModule } from '../shared/shared.module';
+import { TagsEffects } from '../store/effects';
+import { tagsFeature } from '../store/reducers/tags.reducer';
+import { CreateTagDialogComponent } from './components/create-tag-dialog/create-tag-dialog.component';
+import { EditTagDialogComponent } from './components/edit-tag-dialog/edit-tag-dialog.component';
+import { TagsRoutingModule } from './tags-routing.module';
+import { TagsComponent } from './tags.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { EffectsModule } from '@ngrx/effects';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
+    MatExpansionModule,
     StoreModule.forFeature(tagsFeature),
     EffectsModule.forFeature([TagsEffects]),
   ],
