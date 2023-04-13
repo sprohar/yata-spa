96737e13d1349a7470552096d90d71a464dfe56a
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-04-13 00:32:55 -0500

    Added a method to the TasksSortContext to derive the sort strategy from the current state of the orderBy value in the store.

--- src/app/strategies/sort/tasks-sort-context.ts
+++ src/app/strategies/sort/tasks-sort-context.ts
@@ -1,5 +1,8 @@
 import { Task } from '../../models';
+import { TasksOrderByState } from '../../store/reducers/tasks.reducer';
 import { SortStrategy } from './sort.strategy';
+import { TasksDueDateSortStrategy } from './tasks-due-date-sort.strategy';
+import { TasksPrioritySortStrategy } from './tasks-priority-sort.strategy';
 
 export class TasksSortContext {
   private strategy: SortStrategy<Task>;
@@ -15,5 +18,18 @@ export class TasksSortContext {
   setStrategy(strategy: SortStrategy<Task>) {
     this.strategy = strategy;
     return this;
+  }
+
+  fromOrderBy(orderBy: TasksOrderByState | null) {
+    if (orderBy === null) return this;
+
+    switch (orderBy.attribute) {
+      case Task.OrderBy.DUE_DATE:
+        return this.setStrategy(new TasksDueDateSortStrategy(orderBy.dir));
+      case Task.OrderBy.PRIORITY:
+        return this.setStrategy(new TasksPrioritySortStrategy(orderBy.dir));
+      default:
+        return this;
+    }
   }
 }
