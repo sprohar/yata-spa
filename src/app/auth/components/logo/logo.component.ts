d9f66cddc749d2c4a74eef8d466d15282ada3f26
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-04-22 15:44:03 -0500

    Removed the html & scss files for logo component

--- src/app/auth/components/logo/logo.component.ts
+++ src/app/auth/components/logo/logo.component.ts
@@ -2,7 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'yata-logo',
-  styleUrls: ['./logo.component.scss'],
-  templateUrl: './logo.component.html',
+  // styleUrls: ['./logo.component.scss'],
+  template: `
+    <div class="flex">
+      <img id="logo" src="assets/images/logo.png" alt="logo" />
+    </div>
+  `,
+  styles: [
+    `
+      #logo {
+        max-height: inherit;
+        height: 300px;
+        width: auto;
+      }
+
+      @media only screen and (min-width: 600px) {
+        #logo {
+          min-height: 500px;
+          width: auto;
+        }
+      }
+    `,
+  ],
 })
 export class LogoComponent {}
