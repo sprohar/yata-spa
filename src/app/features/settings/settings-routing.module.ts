92fef9f6e07fb9e4597c778da1a44e807f87575e
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-04-22 19:42:53 -0500

    Added the Appearance component to the Settings' routing module

--- src/app/features/settings/settings-routing.module.ts
+++ src/app/features/settings/settings-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppearanceComponent } from './components/appearance/appearance.component';
 import { SettingsComponent } from './settings.component';
 
-const routes: Routes = [{ path: '', component: SettingsComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: SettingsComponent,
+    children: [
+      {
+        path: 'appearance',
+        component: AppearanceComponent,
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: '/app/settings/appearance',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SettingsRoutingModule { }
+export class SettingsRoutingModule {}
--- src/app/features/settings/settings.module.ts
+++ src/app/features/settings/settings.module.ts
@@ -1,17 +1,27 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
-
+import { AppearanceComponent } from './components/appearance/appearance.component';
 
 @NgModule({
-  declarations: [
-    SettingsComponent
-  ],
+  declarations: [SettingsComponent, AppearanceComponent],
   imports: [
     CommonModule,
-    SettingsRoutingModule
-  ]
+    SettingsRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatListModule,
+    MatSlideToggleModule,
+  ],
 })
-export class SettingsModule { }
+export class SettingsModule {}
