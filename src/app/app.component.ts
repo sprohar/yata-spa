4ec7c72d0a42cbbeb4b43ac38c3e549c49944280
Daniel Sprohar <daniel.sprohar@protonmail.com>
2023-04-25 18:04:20 -0500

    Housekeeping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { Store } from '@ngrx/store';
+// import { LocalStorageService } from './services';
+// import { TasksOrderByOptionsActions } from './store/actions';
+// import { TasksOrderByState } from './store/reducers/tasks.reducer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  // constructor(
+  //   private readonly storage: LocalStorageService,
+  //   private readonly store: Store
+  // ) {}
+  //
+  ngOnInit(): void {
+    // const entry: string | null = this.storage.get('orderBy');
+    // if (entry == null) return;
+    //
+    // const orderByState: TasksOrderByState = JSON.parse(entry);
+    // this.store.dispatch(
+    //   TasksOrderByOptionsActions.setOrderBy({
+    //     orderBy: orderByState,
+    //   })
+    // );
+  }
+}
