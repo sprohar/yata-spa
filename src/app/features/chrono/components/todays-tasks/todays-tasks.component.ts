8f0e3cdb76f2a156a6b7ce3dbe5afd684e0233b2
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-04-30 01:14:12 -0500

    Change detection strategy

--- src/app/features/chrono/components/todays-tasks/todays-tasks.component.ts
+++ src/app/features/chrono/components/todays-tasks/todays-tasks.component.ts
@@ -1,27 +1,26 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { TaskView } from '../../../../interfaces';
 import { Task } from '../../../../models';
-import { selectTasks } from '../../../../store/selectors';
-import { PreferencesService } from '../../../preferences/services/preferences.service';
+import {
+  selectTasks,
+  selectUserPreferences,
+} from '../../../../store/selectors';
 
 @Component({
   selector: 'yata-todays-tasks',
   templateUrl: './todays-tasks.component.html',
   styleUrls: ['./todays-tasks.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodaysTasksComponent {
-  readonly userPreferences = this.preferences.get();
   readonly TASK_VIEW_MINIMALIST = TaskView.MINIMALIST;
   readonly TASK_VIEW_INFORMATIVE = TaskView.INFORMATIVE;
+  readonly preferences$ = this.store.select(selectUserPreferences);
+  readonly today = new Date();
+  readonly tasks$ = this.store.select(selectTasks);
 
-  today = new Date();
-  tasks$ = this.store.select(selectTasks);
-
-  constructor(
-    private readonly preferences: PreferencesService,
-    private readonly store: Store
-  ) {}
+  constructor(private readonly store: Store) {}
 
   trackByTaskId(_index: number, task: Task) {
     return task.id;
