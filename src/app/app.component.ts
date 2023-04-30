46826614bcf3d271736aedf6d2e18f088d962c12
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-04-29 22:23:49 -0500

    Removed unused files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-// import { Store } from '@ngrx/store';
-// import { LocalStorageService } from './services';
-// import { TasksOrderByOptionsActions } from './store/actions';
-// import { TasksOrderByState } from './store/reducers/tasks.reducer';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: ` <router-outlet></router-outlet> `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit {
-  // constructor(
-  //   private readonly storage: LocalStorageService,
-  //   private readonly store: Store
-  // ) {}
-  //
-  ngOnInit(): void {
-    // const entry: string | null = this.storage.get('orderBy');
-    // if (entry == null) return;
-    //
-    // const orderByState: TasksOrderByState = JSON.parse(entry);
-    // this.store.dispatch(
-    //   TasksOrderByOptionsActions.setOrderBy({
-    //     orderBy: orderByState,
-    //   })
-    // );
-  }
-}
+export class AppComponent {}
