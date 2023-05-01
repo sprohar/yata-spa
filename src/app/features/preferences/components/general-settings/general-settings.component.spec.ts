d4e665e40eeab120fc0d3d5c5ea95b8b55b7a267
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-01 14:25:04 -0500

    GeneralSettingsComponent: specs

--- src/app/features/preferences/components/general-settings/general-settings.component.spec.ts
+++ src/app/features/preferences/components/general-settings/general-settings.component.spec.ts
@@ -1,4 +1,6 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatCardModule } from '@angular/material/card';
 
 import { GeneralSettingsComponent } from './general-settings.component';
 
@@ -8,9 +10,10 @@ describe('GeneralSettingsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GeneralSettingsComponent ]
-    })
-    .compileComponents();
+      declarations: [GeneralSettingsComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [MatCardModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GeneralSettingsComponent);
     component = fixture.componentInstance;
