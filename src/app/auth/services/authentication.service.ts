a3f865a667ee7328e7f261258aa27303e1a01791
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-05 16:34:35 -0500

    Removed some console.log() statements

--- src/app/auth/services/authentication.service.ts
+++ src/app/auth/services/authentication.service.ts
@@ -92,14 +92,9 @@ export class AuthenticationService {
   private handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
+      // console.error('An error occurred:', error.error);
       return throwError(() => error);
     }
-
-    console.error(
-      `Backend returned code ${error.status}, body was: `,
-      error.error
-    );
 
     return throwError(() => error.error as ApiErrorResponse);
   }
--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,6 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+  .catch((_err) => {
+    // console.error(err);
+  });
