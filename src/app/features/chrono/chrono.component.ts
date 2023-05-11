77833e7d140076baf701c4095e14ca823af65767
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-11 12:17:54 -0500

    CSS changes for consistent padding

--- src/app/features/chrono/chrono.component.ts
+++ src/app/features/chrono/chrono.component.ts
@@ -3,17 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'yata-chrono',
-  styles: [
-    `
-      main {
-        padding: 16px;
-      }
-    `,
-  ],
-  template: `
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-  `,
+  template: ` <router-outlet></router-outlet> `,
 })
 export class ChronoComponent {}
