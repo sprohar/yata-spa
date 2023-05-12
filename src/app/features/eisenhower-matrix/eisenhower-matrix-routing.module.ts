3bbb3d4100a77414e2bc57276e8f8595de2e1681
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-12 12:16:45 -0500

    EMatrix feature: replaced the canActive guard with a resolver.

--- src/app/features/eisenhower-matrix/eisenhower-matrix-routing.module.ts
+++ src/app/features/eisenhower-matrix/eisenhower-matrix-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { taskDetailsResolver } from '../../resolvers/task-details.resolver';
 import { TaskDetailsDialogEntryComponent } from '../../shared/components/task-details-dialog-entry/task-details-dialog-entry.component';
 import { EisenhowerMatrixComponent } from './eisenhower-matrix.component';
-import { eisenhowerMatrixGuard } from './guards/eisenhower-matrix-tasks.guard';
+import { tasksResolver } from './resolvers/matrix.resolver';
 
 const routes: Routes = [
   {
     path: '',
     component: EisenhowerMatrixComponent,
-    canActivate: [eisenhowerMatrixGuard],
+    resolve: {
+      tasks: tasksResolver,
+    },
     children: [
       {
         path: 'p/:projectId/tasks/:taskId',
